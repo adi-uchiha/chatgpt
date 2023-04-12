64f7ce26ee95aa8886c497404df0fe39153d8de4
Aditya Shelke <adi.cs.night@gmail.com>
2023-04-12 18:37:11 +0530

    Chat log add

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,38 @@
+import React from 'react';
 import './App.css';
 import './normal.css';
-function App() {
-  return (
-    <div className="App">
-      <aside className="sidemenu">
-        <div className="new-chat-button">
+function App () {
+	return (
+		<div className="App">
+			<aside className="sidemenu">
+				<div className="new-chat-button">
             + &nbsp; &nbsp; New Chat
-        </div>
-        <div>
+				</div>
+				<div>
           Chats
-        </div>
-      </aside>
+				</div>
+			</aside>
 
-      <section className="chatbox">
-        <div className='chat-input-holder'>
+			<section className="chatbox">
 
-        <textarea className="chat-input-textarea" placeholder="Enter your prompt here" rows='1'>
-          
-        </textarea>
-        </div>
-      </section>
-      
-    </div>
-  );
+				<div className='chat-log'>
+					<div className='chat-message'>
+						<div className='avatar'> </div>
+						<div className='message'>Message</div>
+
+					</div>
+				</div>
+
+				<div className='chat-input-holder'>
+
+					<textarea className="chat-input-textarea" placeholder="Enter your prompt here" rows='1'>
+
+					</textarea>
+				</div>
+			</section>
+
+		</div>
+	);
 }
 
 export default App;
